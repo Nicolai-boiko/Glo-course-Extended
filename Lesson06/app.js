a208bda22cb30d684c1fc7c76181a86d3a3a14da
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-08 12:02:35 +0300

    feat: add functional

--- Lesson06/app.js
+++ Lesson06/app.js
@@ -1,13 +1,15 @@
+function letsPlay () {
+
 let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n)
 };
-
 
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 };
 let randomNumber = randomInteger(1, 100);
+let attempts = 10;
 
 (function start(){
     let number = randomNumber;
@@ -20,14 +22,29 @@ let randomNumber = randomInteger(1, 100);
             start();
         } else {
             if (+enterNumber > number) {
-                alert('Загаданное число меньше');
-                start();
+                --attempts;
+                    if (attempts === 0) {
+                        let tryAgain = confirm('Попытки закончились, хотите сыграть еще?');
+                        tryAgain ? letsPlay () : alert('Игра окончена');
+                    } else {
+                        alert(`Загаданное число меньше, осталось попыток ${attempts}`);
+                        start();
+                    }
             } else if (+enterNumber < number) {
-                alert('Загаданное число больше');
-                start();
+                --attempts;
+                    if (attempts === 0) {
+                        let tryAgain = confirm('Попытки закончились, хотите сыграть еще?');
+                            tryAgain ? letsPlay () : alert('Игра окончена');
+                    } else {
+                        alert(`Загаданное число больше, осталось попыток ${attempts}`);
+                        start();
+                    }
             } else if (+enterNumber === number) {
                 alert('Поздравляю, Вы угадали!!!');
+                let tryAgain = confirm('Хотите сыграть еще?');
+                        tryAgain ? letsPlay () : alert('Игра окончена');
             }
     }
 })();
-
+}
+letsPlay ();
