a571cad418f63d9b66108d0c2e06060df35b86c8
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-10 16:29:21 +0300

    fix: fix weekend days

--- Lesson07/app.js
+++ Lesson07/app.js
@@ -5,13 +5,16 @@ let currentDay = date.getDay();
 function showWeek () {
     for (let i = 0; i < 7; i++) {
         let element = document.createElement('div');
-        element.innerHTML = `${week[i]}`;
-        document.body.append(element);
-        if (i === 5 || i === 6) {
+        if ((i === 5 && i === currentDay - 1) || (i === 6 && i === currentDay - 1)) {
+          element.innerHTML = `<i><strong>${week[i]}</strong></i>`;  
+        } else if (i === 5 || i === 6) {
             element.innerHTML = `<i>${week[i]}</i>`;
         } else if (i === currentDay - 1) {
             element.innerHTML = `<strong>${week[i]}</strong>`;
-        }
+        } else {
+        element.innerHTML = `${week[i]}`;
+        };
+        document.body.append(element);
         element.style.fontFamily = 'sans-serif';
     }
 };
