cc5f40b3f1244772224df72916953cf79af65e89
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-21 18:52:58 +0300

    fix: var to let

--- Lesson18-1/app.js
+++ Lesson18-1/app.js
@@ -2,14 +2,14 @@ let input = document.querySelector('#input');
 let text = document.querySelector('.text');
 
 function debounce(func, wait = 300, immediate = false) {
-    var timeout;
+    let timeout;
     return function() {
-        var context = this, args = arguments;
-        var later = function() {
+        let context = this, args = arguments;
+        let later = function() {
         timeout = null;
         if (!immediate) func.apply(context, args);
         };
-        var callNow = immediate && !timeout;
+        let callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
